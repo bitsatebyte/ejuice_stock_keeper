19e9decae97fa91924242f48e079f33f77b3bca4
Hari Veturi <hariveturi@gmail.com>
2020-01-24 23:26:06 +0530

    Setup react, react-dom, @babel/core babel-loader, @babel/preset-env, @babel/preset-react  html-webpack-plugin, webpack, webpack-dev-server and webpack-cli. Also configured settings for webpack and babel

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+
+// Renders the app component and injects it into the root element
+ReactDOM.render(<App />, document.getElementById('root'));
